ff7d8c1d97bd38542c6ca5388733d17a261abd65
Michael Kovalev <misha.kovalev980@gmail.com>
2025-11-05 14:33:07 -0500

    idk anymore

---
+++ Q2/11.03.25/BubbleSort.java
@@ -0,0 +1,13 @@
+class BubbleSort {
+    public static void main(String[] args) {
+        int array = new int[5];
+
+        for (int i = 0; i<=)
+
+        for (int i = 0; i <= 5; i++) {
+            if (array[i] <= array[i+1]) {
+
+            }
+        }
+    }
+}
---
+++ Q2/11.05.2025/SelectionSort.java
@@ -0,0 +1,5 @@
+class SelectionSort {
+    public static void main(String[] args) {
+        int array[] = {7,8,3,1,2};
+    }
+}
