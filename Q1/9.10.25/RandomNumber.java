39d0a2621005c099e506d66967f5a36e8af7f347
Michael Kovalev <misha.kovalev980@gmail.com>
2025-11-28 09:49:08 -0500

    :)

--- Q1/9.10.25/RandomNumber.java
+++ Q1/9.10.25/RandomNumber.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 
 class RandomNumber {
     public static void main(String[] args) {
-        RandomNumber r = new RandomNumber();
+        Generator r = new Generator();
         int upperBound = r.upperBound();
         int lowerBound = r.lowerBound();
 
--- Q2/11.06.25/AverageArray.java
+++ Q2/11.06.25/AverageArray.java
@@ -2,15 +2,22 @@
 //Use an enhanced for-loop.
 class AverageArray {
     public static void main(String[] args) {
-        for (int a:array) {
-            Syste
-        }
+	int array[] = {1,2,3,4,67};
+	Array a = new Array();
+	int output = a.average(array); 
+    System.out.println(output);  
     }
 }
 
 class Array {
-    int returnArray() {
-        int array[] = {1,2,3,4,5};
-        return array;
-    }
+	int average(int array[]) {
+        int arraySum = 0;
+
+		for (int i:array) {
+			arraySum += i;
+		}
+        
+        int output = arraySum/array.length;
+        return output;
+	}
 }
